76b3747d7bda0f39e73dc99144ea31bf2ae0bd6f
Stephen Eldridge <eldridge.sje@gmail.com>
2021-09-15 13:18:12 -0500

    Updated with PyBank month counts and profit sums

--- PyBank/mainp.py
+++ PyBank/mainp.py
@@ -2,6 +2,10 @@
 import os
 import csv
 
+#declaring variables
+rowCount = 0
+profitSum = 0
+
 #reading CSV
 csvpath = os.path.join('Resources', 'budget_data.csv')
 
@@ -11,3 +15,16 @@ with open(csvpath) as csvfile:
     #printing header
     csv_header = next(csvreader)
     print(f"CSV Header: {csv_header}")
+
+    #looping through the data
+    for row in csvreader:
+    
+        #counting the number of months of data
+        rowCount = (rowCount + 1)
+    
+        #summing the total Profit/Loss
+        profitSum = (profitSum + float(row[1]))
+        
+    #printing results
+    print(f"There are {rowCount} months in the data set.")
+    print(f"The total Profit/Loss is {profitSum}.")
